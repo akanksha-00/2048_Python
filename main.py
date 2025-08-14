5a6abf2260e8e7cf10b0c89cf129b37831e489ae
Akanksha More <akankshamore@Akankshas-Laptop.local>
2025-08-14 13:07:21 +0200

    initial

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+FPS = 60
+
+WIDTH, HEIGHT = 800, 800
+ROWS = 4
+COLUMNS= 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLUMNS
+
+OUTLINE_COLOR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (205, 192, 180)
+FONT_COLOR = (119, 110, 101)
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("2048 Game")
+
+FONT = pygame.font.SysFont("comicsans", 60)
+MOVE_VEL = 20
+
+def main(window):
+    
+    clock = pygame.time.Clock()
+    run = True
+    
+    while run:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            
+    pygame.quit()
+        
+
+if __name__ == "__main__":
+    main(WINDOW)
