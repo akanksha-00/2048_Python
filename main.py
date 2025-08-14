c050f0f323126de7034de5c26cf3959f2d3ac75c
Akanksha More <akankshamore@Akankshas-Laptop.local>
2025-08-14 14:51:35 +0200

    created tile

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ FPS = 60
 
 WIDTH, HEIGHT = 800, 800
 ROWS = 4
-COLUMNS= 4
+COLUMNS = 4
 
 RECT_HEIGHT = HEIGHT // ROWS
 RECT_WIDTH = WIDTH // COLUMNS
@@ -24,21 +24,92 @@ pygame.display.set_caption("2048 Game")
 FONT = pygame.font.SysFont("comicsans", 60)
 MOVE_VEL = 20
 
+
+class Tile:
+    COLORS = [
+        (237, 229, 218),
+        (238, 225, 201),
+        (243, 178, 122),
+        (246, 150, 101),
+        (247, 124, 95),
+        (247, 95, 59),
+        (237, 208, 115),
+        (237, 204, 99),
+        (236, 202, 80),
+    ]
+
+    def __init__(self, value, row, col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+
+    def get_color(self):
+        color_index = int(math.log2(self.value)) - 1
+        color = self.COLORS[color_index]
+        return color
+
+    def draw(self, window):
+        color = self.get_color()
+        pygame.draw.rect(window, color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
+
+        text = FONT.render(str(self.value), 1, FONT_COLOR)
+        window.blit(
+            text,
+            (
+                self.x + (RECT_WIDTH / 2 - text.get_width() / 2),
+                self.y + (RECT_HEIGHT / 2 - text.get_height() / 2),
+            ),
+        )
+
+    def set_pos(self):
+        pass
+
+    def move(self, delta):
+        pass
+
+
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLUMNS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+
+def draw(window, tiles):
+    window.fill(BACKGROUND_COLOR)
+
+    for tile in tiles.values():
+        tile.draw(window)
+
+    draw_grid(window)
+    pygame.display.update()
+
+
 def main(window):
-    
     clock = pygame.time.Clock()
     run = True
-    
+
+    tiles = {}
+
     while run:
         clock.tick(FPS)
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-            
+
+        draw(window, tiles)
+
     pygame.quit()
-        
+
 
 if __name__ == "__main__":
     main(WINDOW)
